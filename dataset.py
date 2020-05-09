b5c72119e7eddfa5dd852560488f1b7f525902d2
Rohit Kumar Singh <rohitku.singh8@gmail.com>
2020-05-10 04:22:24 +0530

    new dataset file

--- dataset.py
+++ dataset.py
@@ -9,9 +9,15 @@ import torch
 import torch.nn.functional as F
 
 from PIL import Image
-from utils.augmentations import horisontal_flip
 from torch.utils.data import Dataset
 import torchvision.transforms as transforms
+
+
+def resize(image, size):
+    image = F.interpolate(
+        image.unsqueeze(0), size=(size, size), mode="nearest"
+    ).squeeze(0)
+    return image
 
 
 class ImageFolder(Dataset):
@@ -59,7 +65,6 @@ class MixUpDataset(Dataset):
         self._mixup_args = beta_values
 
     def __getitem__(self, index):
-
         # ---------
         #  Image
         # ---------
@@ -71,25 +76,34 @@ class MixUpDataset(Dataset):
         if lambd >= 1:
             weights1 = np.ones((box1.shape[0], 1))
             box1 = np.hstack((box1, weights1))
-            height, width, _ = img1.shape
+            _, height, width = img1.shape
             if self.preproc is not None:
-                img_o, target_o = self.preproc(
-                    img1, box1, (self.img_size, self.img_size)
-                )
-            return img_o, target_o
+                img1, box1 = self.preproc(img1, box1, (self.img_size, self.img_size))
+            unmix_img = torch.from_numpy(img1).type(torch.FloatTensor)
+            padded_labels = torch.cat(
+                (
+                    torch.zeros(box1.shape[0], 1),
+                    torch.from_numpy(box1[:, -2].reshape(-1, 1)).type(
+                        torch.FloatTensor
+                    ),
+                    torch.from_numpy(box1[:, :4]).type(torch.FloatTensor),
+                ),
+                dim=1,
+            )
+            return unmix_img, padded_labels
 
         index2 = int(np.random.choice(np.delete(np.arange(len(self)), index)))
         img2, box2 = self.read_img_and_annot(index2)
 
         # mixup two images
-        height = max(img1.shape[0], img2.shape[0])
-        width = max(img1.shape[1], img2.shape[1])
-        mix_img = np.zeros((height, width, 3), dtype=np.float32)
-        mix_img[: img1.shape[0], : img1.shape[1], :] = img1.astype(np.float32) * lambd
-        mix_img[: img2.shape[0], : img2.shape[1], :] += img2.astype(np.float32) * (
+        height = max(img1.shape[1], img2.shape[1])
+        width = max(img1.shape[2], img2.shape[2])
+        mix_img = np.zeros((3, height, width), dtype=np.float32)
+        mix_img[:, : img1.shape[1], : img1.shape[2]] = img1.astype(np.float32) * lambd
+        mix_img[:, : img2.shape[1], : img2.shape[2]] += img2.astype(np.float32) * (
             1.0 - lambd
         )
-        mix_img = mix_img.astype(np.uint8)
+        # mix_img = mix_img.astype(np.uint8)
 
         y1 = np.hstack((box1, np.full((box1.shape[0], 1), lambd)))
         y2 = np.hstack((box2, np.full((box2.shape[0], 1), 1.0 - lambd)))
@@ -99,8 +113,16 @@ class MixUpDataset(Dataset):
                 mix_img, mix_label, (self.img_size, self.img_size)
             )
 
+        mix_img = torch.from_numpy(mix_img).type(torch.FloatTensor)
         padded_labels = torch.cat(
-            (torch.zeros((padded_labels.shape[0], 1)), padded_labels.float()), dim=1
+            (
+                torch.zeros(mix_label.shape[0], 1),
+                torch.from_numpy(mix_label[:, -2].reshape(-1, 1)).type(
+                    torch.FloatTensor
+                ),
+                torch.from_numpy(mix_label[:, :4]).type(torch.FloatTensor),
+            ),
+            dim=1,
         )
 
         return mix_img, padded_labels
@@ -117,7 +139,10 @@ class MixUpDataset(Dataset):
         line = line[1].split(" ")
         iw, ih = img.size
         h, w = self.img_size, self.img_size
-        box = np.array([np.array(list(map(int, box.split(",")))) for box in line[1:]])
+        box = np.array(
+            [np.array(list(map(int, box.split(",")))) for box in line[1:]],
+            dtype=np.float32,
+        )
 
         # resize image
         scale = min(w / iw, h / ih)
@@ -128,18 +153,25 @@ class MixUpDataset(Dataset):
         img = img.resize((nw, nh), Image.BICUBIC)
         new_image = Image.new("RGB", (w, h), (128, 128, 128))
         new_image.paste(img, (dx, dy))
-        img = np.array(new_image)
+        img = np.array(new_image).reshape((3, h, w))
 
-        # correct boxes
+        # correct boxesa
         if len(box) > 0:
-            np.random.shuffle(box)
+            #             np.random.shuffle(box)
             box[:, [0, 2]] = box[:, [0, 2]] * scale + dx
             box[:, [1, 3]] = box[:, [1, 3]] * scale + dy
-
+            b_x = (box[:, 2] + box[:, 0]) * 0.5
+            b_y = (box[:, 3] + box[:, 1]) * 0.5
+            b_w = (box[:, 2] - box[:, 0]) * 1.0
+            b_h = (box[:, 3] - box[:, 1]) * 1.0
+            box[:, 0] = b_x / w  # *(w/iw)
+            box[:, 1] = b_y / h  # *(h/ih)
+            box[:, 2] = b_w / w  # *(w/iw)
+            box[:, 3] = b_h / h  # *(w/iw)
         return img, box
 
     def collate_fn(self, batch):
-        paths, imgs, targets = list(zip(*batch))
+        imgs, targets = list(zip(*batch))
         # Remove empty placeholder targets
         targets = [boxes for boxes in targets if boxes is not None]
         # Add sample index to targets
@@ -150,9 +182,9 @@ class MixUpDataset(Dataset):
         if self.multiscale and self.batch_count % 10 == 0:
             self.img_size = random.choice(range(self.min_size, self.max_size + 1, 32))
         # Resize images to input shape
-        imgs = torch.stack([resize(img, self.img_size) for img in imgs])
+        imgs = torch.stack([img for img in imgs])
         self.batch_count += 1
-        return paths, imgs, targets
+        return imgs, targets
 
     def __len__(self):
         return len(self.img_files)
